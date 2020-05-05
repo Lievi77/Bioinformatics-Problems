d0bd36fed49266d2ebc06e77d1aa5cd3abc31ed9
Lievi77 <lguzm038@uottawa.ca>
2020-05-05 14:28:54 -0400

    Changed aprroach to Suffix Array

--- Problem 12: Finding a Shared Motif/common_motif.py
+++ Problem 12: Finding a Shared Motif/common_motif.py
@@ -1,5 +1,5 @@
 import common_methods as cm
-from collections import OrderedDict
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -11,54 +11,75 @@ Created on Sun May  3 13:31:32 2020
 """
 #------------------------------METHODS-----------------------------------------
 
-def is_common(substring, dna_map, current_dna):
-    
-    verdict = None
-    
-    for dna in dna_map.values():
-        
-        if dna == current_dna:
-            continue
-        
-        verdict = substring in dna
-        
-        if not verdict:
-            return False
+def build_sa(string): #build the suffix arrays. 
+    pass
 
-    
-    return verdict
+def decode(string, rules): #decodes the string using rules
 
-def find_longest_common_sub_dna(dna_map):
+    pass
     
-    longest_sub_dna = "" #empty string initialization
+
+def largest_common_substring(dna_list): 
     
+    #we will use a suffix array approach
+    #since our input can be up tp 100 dna strands
+    #dynamic programming can quickly get out of hand
     
-    for dna in dna_map.values(): #loop through dna_strings
+    #first step, concatenate the strings and use unique sentinels
+    #map alphabet to natural numbers
+    #shift up for the number of sentinel characters you need
+    
+    big_dna = ""
+    alphabet_map = {} #order does not matter { mapped_value : CHAR ITS referring to }
+    
+    #building the map, 
+    alphabet_map[chr(200)]= "A"
+    alphabet_map[chr(220)] ="T"
+    alphabet_map[chr(240)] = "C"
+    alphabet_map[chr(260)] = "G"
+    
+    #build the big_dna string using the map defined above
+    
+    for i in range(len(dna_list)):
+        coded_dna = ""
         
-        substring = ""
+        for char in dna_list[i]: #loop to code the dna string
+            
+            if char == "A":
+                coded_dna += chr(200)
+            elif char == "T":
+                coded_dna += chr(220)
+            elif char == "C":
+                coded_dna += chr(240)
+            else:
+                coded_dna += chr(260)
+        
+        if i == 0: #first instance does not have a sentinel in front
+            big_dna += coded_dna
+        else:
+            sentinel = chr(i + 33) #sentinels go from [33,132]
+            big_dna += sentinel + coded_dna
+            
+    print(big_dna)
     
-        #the next loop generates every single possible substring
-        #must change to so that substring goes from largest to smallest
-        
-        
-                    
-                    
-                    
-    return longest_sub_dna
+    
+    
+    #next, build a suffix array
+    build_sa(big_dna)
+    
+    
+            
+    
 #-------------------------------MAIN-----------------------------------------
 
 def main():
-    dna_map = cm.process_dna_file(open("rosalind_lcsm.txt","r")) #ORDERED DICTIONARY
+    dna_map = cm.process_dna_file(open("test.txt","r")) #ORDERED DICTIONARY
     
+    dna_list = cm.get_dna_strings(dna_map)
     
-    longest_motif = find_longest_common_sub_dna(dna_map)
+    output = largest_common_substring(dna_list)
     
+    print(output)
     
-    print(longest_motif)
-
 #execute main method
 main()
-
-
-
-
