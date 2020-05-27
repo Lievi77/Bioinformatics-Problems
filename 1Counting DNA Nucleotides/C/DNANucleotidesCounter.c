eaff66398b028c9cd35c81e8040efa439dbc16d5
Lievi77 <lguzm038@uottawa.ca>
2020-05-27 16:20:43 -0400

    Repository update

---
+++ 1Counting DNA Nucleotides/C/DNANucleotidesCounter.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //always start a program with this include
+
+int main()
+{ //main method declaration, similar to java
+
+    
+    return 0; //in C, the main method returns 0 for a successful process, 1 for failure
+}
